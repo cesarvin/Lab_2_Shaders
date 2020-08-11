00ecbb7ae7780431c909c1021f9faabf03da7af7
cesarvin <vinizio007@gmail.com>
2020-08-10 18:26:03 -0600

    Lab2

---
+++ test.py
@@ -0,0 +1,11 @@
+from gl import Render, color, V2, V3
+from obj import Obj, Texture
+
+r = Render()
+r.glCreateWindow(1000,1000)
+
+t = Texture('./models/model.bmp')
+r.glLoadModel('./models/model.obj',V3(500,500,0), V3(300,300,300), t)
+
+r.glFinish('output.bmp')
+
