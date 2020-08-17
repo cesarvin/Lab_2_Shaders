67da8302206bcd82c7381405469e440bf5fe63f9
cesarvin <vinizio007@gmail.com>
2020-08-16 23:20:19 -0600

    toon y static_matrix

--- test.py
+++ test.py
@@ -11,9 +11,19 @@ r.glCreateWindow(1000,1000)
 r.active_texture = Texture('./models/model.bmp')
 
 r.active_shader = gourad
-
 #r.glLoadModel('./models/earth.obj', V3(500,500,0), V3(1,1,1))
-
 r.glLoadModel('./models/model.obj',V3(500,500,0), V3(300,300,300))
-
 r.glFinish('gourad.bmp')
+print('gourad.bmp creado')
+r.glClear()
+
+r.active_shader = toon
+r.glLoadModel('./models/model.obj',V3(500,500,0), V3(300,300,300))
+r.glFinish('toon.bmp')
+print('toon.bmp creado')
+r.glClear()
+
+r.active_shader = static_matrix
+r.glLoadModel('./models/model.obj',V3(500,500,0), V3(300,300,300))
+print('static_matrix.bmp creado')
+r.glFinish('static_matrix.bmp')
